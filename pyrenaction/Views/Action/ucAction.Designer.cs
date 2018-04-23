01b77945333ea8cc30a8d6a74e39edab142f60ed
Zinkou <cousin.maxime@laposte.net>
2018-04-23 11:58:37 +0200

    View Connexion, ModifAction et SQL

---
+++ pyrenaction/Views/Action/ucAction.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace pyrenaction.Views.Action
+{
+    partial class ucAction
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitre = new System.Windows.Forms.Label();
+            this.inputNumero = new System.Windows.Forms.TextBox();
+            this.labelNumero = new System.Windows.Forms.Label();
+            this.inputDate = new System.Windows.Forms.TextBox();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.inputSource = new System.Windows.Forms.RichTextBox();
+            this.labelSource = new System.Windows.Forms.Label();
+            this.labelAnalyse = new System.Windows.Forms.Label();
+            this.inputAnalyse = new System.Windows.Forms.RichTextBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.inputDescription = new System.Windows.Forms.RichTextBox();
+            this.inputStatut = new System.Windows.Forms.CheckBox();
+            this.labelStatut = new System.Windows.Forms.Label();
+            this.inputImportance = new System.Windows.Forms.ComboBox();
+            this.labelImportance = new System.Windows.Forms.Label();
+            this.labelFamille = new System.Windows.Forms.Label();
+            this.inputFamille = new System.Windows.Forms.ComboBox();
+            this.labelSite = new System.Windows.Forms.Label();
+            this.inputSite = new System.Windows.Forms.ComboBox();
+            this.labelResponsable1 = new System.Windows.Forms.Label();
+            this.inputResponsable1 = new System.Windows.Forms.ComboBox();
+            this.labelResponsable2 = new System.Windows.Forms.Label();
+            this.inputResponsable2 = new System.Windows.Forms.ComboBox();
+            this.labelAction = new System.Windows.Forms.Label();
+            this.inputAction = new System.Windows.Forms.ComboBox();
+            this.labelQuestionnaire = new System.Windows.Forms.Label();
+            this.inputQuestionnaire = new System.Windows.Forms.ComboBox();
+            this.labelDelais = new System.Windows.Forms.Label();
+            this.inputDelais = new System.Windows.Forms.TextBox();
+            this.buttonAjouter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitre
+            // 
+            this.labelTitre.AutoSize = true;
+            this.labelTitre.Location = new System.Drawing.Point(420, 39);
+            this.labelTitre.Name = "labelTitre";
+            this.labelTitre.Size = new System.Drawing.Size(123, 17);
+            this.labelTitre.TabIndex = 0;
+            this.labelTitre.Text = "Ajouter une action";
+            this.labelTitre.Click += new System.EventHandler(this.labelTitre_Click);
+            // 
+            // inputNumero
+            // 
+            this.inputNumero.Location = new System.Drawing.Point(196, 119);
+            this.inputNumero.Name = "inputNumero";
+            this.inputNumero.Size = new System.Drawing.Size(259, 22);
+            this.inputNumero.TabIndex = 1;
+            // 
+            // labelNumero
+            // 
+            this.labelNumero.AutoSize = true;
+            this.labelNumero.Location = new System.Drawing.Point(75, 119);
+            this.labelNumero.Name = "labelNumero";
+            this.labelNumero.Size = new System.Drawing.Size(24, 17);
+            this.labelNumero.TabIndex = 2;
+            this.labelNumero.Text = "N°";
+            // 
+            // inputDate
+            // 
+            this.inputDate.Location = new System.Drawing.Point(196, 177);
+            this.inputDate.Name = "inputDate";
+            this.inputDate.Size = new System.Drawing.Size(259, 22);
+            this.inputDate.TabIndex = 3;
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(75, 177);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(38, 17);
+            this.labelDate.TabIndex = 4;
+            this.labelDate.Text = "Date";
+            // 
+            // inputSource
+            // 
+            this.inputSource.Location = new System.Drawing.Point(685, 119);
+            this.inputSource.Name = "inputSource";
+            this.inputSource.Size = new System.Drawing.Size(259, 96);
+            this.inputSource.TabIndex = 11;
+            this.inputSource.Text = "";
+            // 
+            // labelSource
+            // 
+            this.labelSource.AutoSize = true;
+            this.labelSource.Location = new System.Drawing.Point(564, 119);
+            this.labelSource.Name = "labelSource";
+            this.labelSource.Size = new System.Drawing.Size(53, 17);
+            this.labelSource.TabIndex = 12;
+            this.labelSource.Text = "Source";
+            // 
+            // labelAnalyse
+            // 
+            this.labelAnalyse.AutoSize = true;
+            this.labelAnalyse.Location = new System.Drawing.Point(564, 221);
+            this.labelAnalyse.Name = "labelAnalyse";
+            this.labelAnalyse.Size = new System.Drawing.Size(58, 17);
+            this.labelAnalyse.TabIndex = 14;
+            this.labelAnalyse.Text = "Analyse";
+            // 
+            // inputAnalyse
+            // 
+            this.inputAnalyse.Location = new System.Drawing.Point(685, 221);
+            this.inputAnalyse.Name = "inputAnalyse";
+            this.inputAnalyse.Size = new System.Drawing.Size(259, 96);
+            this.inputAnalyse.TabIndex = 13;
+            this.inputAnalyse.Text = "";
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(564, 324);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(79, 17);
+            this.labelDescription.TabIndex = 16;
+            this.labelDescription.Text = "Description";
+            // 
+            // inputDescription
+            // 
+            this.inputDescription.Location = new System.Drawing.Point(685, 324);
+            this.inputDescription.Name = "inputDescription";
+            this.inputDescription.Size = new System.Drawing.Size(259, 96);
+            this.inputDescription.TabIndex = 15;
+            this.inputDescription.Text = "";
+            // 
+            // inputStatut
+            // 
+            this.inputStatut.Location = new System.Drawing.Point(196, 413);
+            this.inputStatut.Name = "inputStatut";
+            this.inputStatut.Size = new System.Drawing.Size(98, 21);
+            this.inputStatut.TabIndex = 0;
+            this.inputStatut.UseVisualStyleBackColor = true;
+            // 
+            // labelStatut
+            // 
+            this.labelStatut.AutoSize = true;
+            this.labelStatut.Location = new System.Drawing.Point(75, 413);
+            this.labelStatut.Name = "labelStatut";
+            this.labelStatut.Size = new System.Drawing.Size(45, 17);
+            this.labelStatut.TabIndex = 17;
+            this.labelStatut.Text = "Statut";
+            // 
+            // inputImportance
+            // 
+            this.inputImportance.FormattingEnabled = true;
+            this.inputImportance.Location = new System.Drawing.Point(196, 233);
+            this.inputImportance.Name = "inputImportance";
+            this.inputImportance.Size = new System.Drawing.Size(259, 24);
+            this.inputImportance.TabIndex = 19;
+            // 
+            // labelImportance
+            // 
+            this.labelImportance.AutoSize = true;
+            this.labelImportance.Location = new System.Drawing.Point(75, 233);
+            this.labelImportance.Name = "labelImportance";
+            this.labelImportance.Size = new System.Drawing.Size(78, 17);
+            this.labelImportance.TabIndex = 20;
+            this.labelImportance.Text = "Importance";
+            // 
+            // labelFamille
+            // 
+            this.labelFamille.AutoSize = true;
+            this.labelFamille.Location = new System.Drawing.Point(75, 263);
+            this.labelFamille.Name = "labelFamille";
+            this.labelFamille.Size = new System.Drawing.Size(52, 17);
+            this.labelFamille.TabIndex = 22;
+            this.labelFamille.Text = "Famille";
+            // 
+            // inputFamille
+            // 
+            this.inputFamille.FormattingEnabled = true;
+            this.inputFamille.Location = new System.Drawing.Point(196, 263);
+            this.inputFamille.Name = "inputFamille";
+            this.inputFamille.Size = new System.Drawing.Size(259, 24);
+            this.inputFamille.TabIndex = 21;
+            // 
+            // labelSite
+            // 
+            this.labelSite.AutoSize = true;
+            this.labelSite.Location = new System.Drawing.Point(75, 293);
+            this.labelSite.Name = "labelSite";
+            this.labelSite.Size = new System.Drawing.Size(32, 17);
+            this.labelSite.TabIndex = 24;
+            this.labelSite.Text = "Site";
+            // 
+            // inputSite
+            // 
+            this.inputSite.FormattingEnabled = true;
+            this.inputSite.Location = new System.Drawing.Point(196, 293);
+            this.inputSite.Name = "inputSite";
+            this.inputSite.Size = new System.Drawing.Size(259, 24);
+            this.inputSite.TabIndex = 23;
+            // 
+            // labelResponsable1
+            // 
+            this.labelResponsable1.AutoSize = true;
+            this.labelResponsable1.Location = new System.Drawing.Point(75, 323);
+            this.labelResponsable1.Name = "labelResponsable1";
+            this.labelResponsable1.Size = new System.Drawing.Size(103, 17);
+            this.labelResponsable1.TabIndex = 26;
+            this.labelResponsable1.Text = "Responsable 1";
+            // 
+            // inputResponsable1
+            // 
+            this.inputResponsable1.FormattingEnabled = true;
+            this.inputResponsable1.Location = new System.Drawing.Point(196, 323);
+            this.inputResponsable1.Name = "inputResponsable1";
+            this.inputResponsable1.Size = new System.Drawing.Size(259, 24);
+            this.inputResponsable1.TabIndex = 25;
+            // 
+            // labelResponsable2
+            // 
+            this.labelResponsable2.AutoSize = true;
+            this.labelResponsable2.Location = new System.Drawing.Point(75, 353);
+            this.labelResponsable2.Name = "labelResponsable2";
+            this.labelResponsable2.Size = new System.Drawing.Size(103, 17);
+            this.labelResponsable2.TabIndex = 28;
+            this.labelResponsable2.Text = "Responsable 2";
+            // 
+            // inputResponsable2
+            // 
+            this.inputResponsable2.FormattingEnabled = true;
+            this.inputResponsable2.Location = new System.Drawing.Point(196, 353);
+            this.inputResponsable2.Name = "inputResponsable2";
+            this.inputResponsable2.Size = new System.Drawing.Size(259, 24);
+            this.inputResponsable2.TabIndex = 27;
+            // 
+            // labelAction
+            // 
+            this.labelAction.AutoSize = true;
+            this.labelAction.Location = new System.Drawing.Point(75, 147);
+            this.labelAction.Name = "labelAction";
+            this.labelAction.Size = new System.Drawing.Size(73, 17);
+            this.labelAction.TabIndex = 30;
+            this.labelAction.Text = "Action liée";
+            // 
+            // inputAction
+            // 
+            this.inputAction.FormattingEnabled = true;
+            this.inputAction.Location = new System.Drawing.Point(196, 147);
+            this.inputAction.Name = "inputAction";
+            this.inputAction.Size = new System.Drawing.Size(259, 24);
+            this.inputAction.TabIndex = 29;
+            // 
+            // labelQuestionnaire
+            // 
+            this.labelQuestionnaire.AutoSize = true;
+            this.labelQuestionnaire.Location = new System.Drawing.Point(75, 383);
+            this.labelQuestionnaire.Name = "labelQuestionnaire";
+            this.labelQuestionnaire.Size = new System.Drawing.Size(97, 17);
+            this.labelQuestionnaire.TabIndex = 32;
+            this.labelQuestionnaire.Text = "Questionnaire";
+            // 
+            // inputQuestionnaire
+            // 
+            this.inputQuestionnaire.FormattingEnabled = true;
+            this.inputQuestionnaire.Location = new System.Drawing.Point(196, 383);
+            this.inputQuestionnaire.Name = "inputQuestionnaire";
+            this.inputQuestionnaire.Size = new System.Drawing.Size(259, 24);
+            this.inputQuestionnaire.TabIndex = 31;
+            // 
+            // labelDelais
+            // 
+            this.labelDelais.AutoSize = true;
+            this.labelDelais.Location = new System.Drawing.Point(75, 205);
+            this.labelDelais.Name = "labelDelais";
+            this.labelDelais.Size = new System.Drawing.Size(47, 17);
+            this.labelDelais.TabIndex = 34;
+            this.labelDelais.Text = "Délais";
+            // 
+            // inputDelais
+            // 
+            this.inputDelais.Location = new System.Drawing.Point(196, 205);
+            this.inputDelais.Name = "inputDelais";
+            this.inputDelais.Size = new System.Drawing.Size(259, 22);
+            this.inputDelais.TabIndex = 33;
+            // 
+            // buttonAjouter
+            // 
+            this.buttonAjouter.Location = new System.Drawing.Point(440, 466);
+            this.buttonAjouter.Name = "buttonAjouter";
+            this.buttonAjouter.Size = new System.Drawing.Size(105, 23);
+            this.buttonAjouter.TabIndex = 35;
+            this.buttonAjouter.Text = "Ajouter";
+            this.buttonAjouter.UseVisualStyleBackColor = true;
+            // 
+            // ucAction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonAjouter);
+            this.Controls.Add(this.labelDelais);
+            this.Controls.Add(this.inputDelais);
+            this.Controls.Add(this.labelQuestionnaire);
+            this.Controls.Add(this.inputQuestionnaire);
+            this.Controls.Add(this.labelAction);
+            this.Controls.Add(this.inputAction);
+            this.Controls.Add(this.labelResponsable2);
+            this.Controls.Add(this.inputResponsable2);
+            this.Controls.Add(this.labelResponsable1);
+            this.Controls.Add(this.inputResponsable1);
+            this.Controls.Add(this.labelSite);
+            this.Controls.Add(this.inputSite);
+            this.Controls.Add(this.labelFamille);
+            this.Controls.Add(this.inputFamille);
+            this.Controls.Add(this.labelImportance);
+            this.Controls.Add(this.inputImportance);
+            this.Controls.Add(this.labelStatut);
+            this.Controls.Add(this.inputStatut);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.inputDescription);
+            this.Controls.Add(this.labelAnalyse);
+            this.Controls.Add(this.inputAnalyse);
+            this.Controls.Add(this.labelSource);
+            this.Controls.Add(this.inputSource);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.inputDate);
+            this.Controls.Add(this.labelNumero);
+            this.Controls.Add(this.inputNumero);
+            this.Controls.Add(this.labelTitre);
+            this.Name = "ucAction";
+            this.Size = new System.Drawing.Size(997, 533);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitre;
+        private System.Windows.Forms.TextBox inputNumero;
+        private System.Windows.Forms.Label labelNumero;
+        private System.Windows.Forms.TextBox inputDate;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.RichTextBox inputSource;
+        private System.Windows.Forms.Label labelSource;
+        private System.Windows.Forms.Label labelAnalyse;
+        private System.Windows.Forms.RichTextBox inputAnalyse;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.RichTextBox inputDescription;
+        private System.Windows.Forms.CheckBox inputStatut;
+        private System.Windows.Forms.Label labelStatut;
+        private System.Windows.Forms.ComboBox inputImportance;
+        private System.Windows.Forms.Label labelImportance;
+        private System.Windows.Forms.Label labelFamille;
+        private System.Windows.Forms.ComboBox inputFamille;
+        private System.Windows.Forms.Label labelSite;
+        private System.Windows.Forms.ComboBox inputSite;
+        private System.Windows.Forms.Label labelResponsable1;
+        private System.Windows.Forms.ComboBox inputResponsable1;
+        private System.Windows.Forms.Label labelResponsable2;
+        private System.Windows.Forms.ComboBox inputResponsable2;
+        private System.Windows.Forms.Label labelAction;
+        private System.Windows.Forms.ComboBox inputAction;
+        private System.Windows.Forms.Label labelQuestionnaire;
+        private System.Windows.Forms.ComboBox inputQuestionnaire;
+        private System.Windows.Forms.Label labelDelais;
+        private System.Windows.Forms.TextBox inputDelais;
+        private System.Windows.Forms.Button buttonAjouter;
+    }
+}
---
+++ pyrenaction/Views/Connexion/ucConnexion.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace pyrenaction.Views.Connexion
+{
+    partial class ucConnexion
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnexion = new System.Windows.Forms.Button();
+            this.labelMdp = new System.Windows.Forms.Label();
+            this.inputMdp = new System.Windows.Forms.TextBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.inputLogin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonConnexion
+            // 
+            this.buttonConnexion.Location = new System.Drawing.Point(236, 223);
+            this.buttonConnexion.Name = "buttonConnexion";
+            this.buttonConnexion.Size = new System.Drawing.Size(87, 23);
+            this.buttonConnexion.TabIndex = 46;
+            this.buttonConnexion.Text = "Connexion";
+            this.buttonConnexion.UseVisualStyleBackColor = true;
+            // 
+            // labelMdp
+            // 
+            this.labelMdp.AutoSize = true;
+            this.labelMdp.Location = new System.Drawing.Point(78, 151);
+            this.labelMdp.Name = "labelMdp";
+            this.labelMdp.Size = new System.Drawing.Size(93, 17);
+            this.labelMdp.TabIndex = 45;
+            this.labelMdp.Text = "Mot de passe";
+            // 
+            // inputMdp
+            // 
+            this.inputMdp.Location = new System.Drawing.Point(199, 151);
+            this.inputMdp.Name = "inputMdp";
+            this.inputMdp.Size = new System.Drawing.Size(259, 22);
+            this.inputMdp.TabIndex = 44;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(78, 123);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(43, 17);
+            this.labelLogin.TabIndex = 43;
+            this.labelLogin.Text = "Login";
+            // 
+            // inputLogin
+            // 
+            this.inputLogin.Location = new System.Drawing.Point(199, 123);
+            this.inputLogin.Name = "inputLogin";
+            this.inputLogin.Size = new System.Drawing.Size(259, 22);
+            this.inputLogin.TabIndex = 42;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(233, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "Connexion";
+            // 
+            // ucConnexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonConnexion);
+            this.Controls.Add(this.labelMdp);
+            this.Controls.Add(this.inputMdp);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.inputLogin);
+            this.Controls.Add(this.label1);
+            this.Name = "ucConnexion";
+            this.Size = new System.Drawing.Size(592, 320);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnexion;
+        private System.Windows.Forms.Label labelMdp;
+        private System.Windows.Forms.TextBox inputMdp;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.TextBox inputLogin;
+        private System.Windows.Forms.Label label1;
+    }
+}
