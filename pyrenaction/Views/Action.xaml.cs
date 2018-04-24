e490a7b875993e87c4dd9e5e3536e8959fc80916
Zinkou <cousin.maxime@laposte.net>
2018-04-24 16:02:50 +0200

    Envoi de mail à la création d'une action

--- pyrenaction/Views/Action.xaml.cs
+++ pyrenaction/Views/Action.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Mail;
 
 namespace pyrenaction.Views
 {
@@ -35,6 +36,29 @@ namespace pyrenaction.Views
         private void Submit(object sender, RoutedEventArgs e)
         {
             _actionController.Valider();
+
+            //Envoi de mail au responsable
+            Char delimiter = ' ';
+            String res = _actionController.Resp1Selected.ToString().Split(delimiter)[0];
+            using (Models.pyrenactionEntities context = new Models.pyrenactionEntities())
+            {
+                var queryDest = from U in context.Utilisateurs where U.nom == res select U;
+                Models.Utilisateur Dest = queryDest.FirstOrDefault();
+                try
+                {
+                    String objet = "Nouvelle action N° " + Dest.id.ToString();
+                    String contenu = "Une nouvelle action a été créée, merci de bien vouloir la traiter. \n Numéro : " + Dest.id.ToString();
+                    MailMessage mail = new MailMessage("pyrenaction@gmail.com", Dest.email, objet, contenu);
+                    SmtpClient stpc = new SmtpClient("smtp.gmail.com", 587);
+                    stpc.Credentials = new System.Net.NetworkCredential("pyrenaction@gmail.com", "Cesi1234%");
+                    stpc.EnableSsl = true;
+                    stpc.Send(mail);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("L'email n'a pu être envoyé : {0}.", ex.Message);
+                }
+            }
         }
 
         private void AjouterTache(object sender, RoutedEventArgs e)
