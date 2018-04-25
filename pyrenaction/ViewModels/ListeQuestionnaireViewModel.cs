8f26b654074d1ea60f79039d86db3d6b917b8d45
Zinkou <cousin.maxime@laposte.net>
2018-04-25 14:38:22 +0200

    Ajout ListeQuestionnaire

---
+++ pyrenaction/ViewModels/ListeQuestionnaireViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyrenaction.ViewModels
+{
+    class ListeQuestionnaireViewModel : ViewModelBase
+    {
+        private Models.Questionnaire _listeQuestionnaire;
+        private Models.pyrenactionEntities _context;
+    }
+}
